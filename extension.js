142a66e7aa7716363b411e236d7a839c523c746c
Maciejka <maciekglab0812@vp.pl>
2025-05-21 19:58:26 +0200

    Rewriting whole component since it didnt work. New icons used, new disconnection detection writen, bugs fixed.

--- extension.js
+++ extension.js
@@ -1,117 +1,159 @@
-// extension.js
-
 import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 import St from 'gi://St';
 
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import GLib from 'gi://GLib';
 
-const SUNSHINE_COMMAND = 'sunshine';
-const SUNSHINE_PROCESS_NAME = 'sunshine';
-
-const STATUS = {
-    OFF: 'off',
-    STARTED: 'started',
-    CONNECTED: 'connected'
-};
-
-const ICONS = {
-    [STATUS.OFF]: 'media-playback-stop-symbolic',
-    [STATUS.STARTED]: 'media-playback-start-symbolic',
-    [STATUS.CONNECTED]: 'network-transmit-receive-symbolic'
-};
-
-const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
+const SunshineIndicator = GObject.registerClass(
+class SunshineIndicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, _('Sunshine Status'));
 
         this._icon = new St.Icon({
-            icon_name: ICONS[STATUS.OFF],
-            style_class: 'system-status-icon',
+            icon_name: 'network-offline-symbolic',
+            style_class: 'sunshine-icon-off',
         });
-
         this.add_child(this._icon);
-        this._status = STATUS.OFF;
 
-        this._startItem = new PopupMenu.PopupMenuItem(_('Start Sunshine'));
-        this._stopItem = new PopupMenu.PopupMenuItem(_('Stop Sunshine'));
+        this._process = null;
+        this._running = false;
+        this._connected = false;
 
-        this._startItem.connect('activate', () => this._startSunshine());
-        this._stopItem.connect('activate', () => this._stopSunshine());
-
-        this.menu.addMenuItem(this._startItem);
-        this.menu.addMenuItem(this._stopItem);
-
-        this._refreshStatus();
-        this._updateMenu();
-
-        this._statusCheckLoop();
+        this._buildMenu();
     }
 
-    _refreshStatus() {
-        const [ok, out] = GLib.spawn_command_line_sync(`pgrep -x ${SUNSHINE_PROCESS_NAME}`);
-        if (ok && out.length > 0) {
-            this._status = this._isClientConnected() ? STATUS.CONNECTED : STATUS.STARTED;
-        } else {
-            this._status = STATUS.OFF;
+    _buildMenu() {
+        this.menu.removeAll();
+
+        let statusText = _('Off');
+        let styleClass = 'sunshine-status-off';
+        if (this._running) {
+            statusText = this._connected ? _('Connected') : _('Started');
+            styleClass = this._connected ? 'sunshine-status-connected' : 'sunshine-status-started';
         }
-        this._updateIcon();
-        this._updateMenu();
+        let statusItem = new PopupMenu.PopupMenuItem(statusText, { reactive: false });
+        statusItem.label.add_style_class_name(styleClass);
+        this.menu.addMenuItem(statusItem);
+
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        let actionText = this._running ? _('Stop Sunshine') : _('Start Sunshine');
+        let actionItem = new PopupMenu.PopupMenuItem(actionText);
+        actionItem.connect('activate', () => this._toggle());
+        this.menu.addMenuItem(actionItem);
     }
 
-    _updateIcon() {
-        this._icon.icon_name = ICONS[this._status];
-        this._icon.style_class = `system-status-icon ${this._status}`;
-    }
-
-    _updateMenu() {
-        this._startItem.visible = this._status === STATUS.OFF;
-        this._stopItem.visible = this._status !== STATUS.OFF;
+    _toggle() {
+        if (!this._running) {
+            this._startSunshine();
+        } else {
+            this._stopSunshine();
+        }
     }
 
     _startSunshine() {
-        GLib.spawn_command_line_async(SUNSHINE_COMMAND);
-        console.info('Sunshine started');
-        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 2, () => {
-            this._refreshStatus();
-            return GLib.SOURCE_REMOVE;
-        });
+        try {
+            this._process = new Gio.Subprocess({
+                argv: ['sunshine'],
+                flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_MERGE,
+            });
+            this._process.init(null);
+            this._running = true;
+            this._connected = false;
+
+            this._process.wait_async(null, (obj, res) => {
+                try {
+                    this._process.wait_finish(res);
+                    this._running = false;
+                    this._connected = false;
+                    this._updateVisuals();
+                } catch (e) {
+                    console.error(`Error waiting for subprocess: ${e}`);
+                }
+            });
+
+            let stdout = new Gio.DataInputStream({ base_stream: this._process.get_stdout_pipe() });
+            const readLine = () => {
+                stdout.read_line_async(GLib.PRIORITY_DEFAULT, null, (obj, res) => {
+                    try {
+                        let [line, length] = obj.read_line_finish(res);
+                        if (line) {
+                            let str = line.toString().toLowerCase();
+                            console.log('Sunshine output: ' + str); 
+                            if (str.includes('client connected')) {
+                                this._connected = true;
+                                this._updateVisuals();
+                            } else if (str.includes('client disconnected')) {
+                                this._connected = false;
+                                this._updateVisuals();
+                            }
+                        }
+                    } catch (e) {
+                        this._running = false;
+                        this._connected = false;
+                        this._updateVisuals();
+                        return;
+                    }
+                    if (this._running)
+                        readLine();
+                });
+            };
+            readLine();
+
+            this._updateVisuals();
+        } catch (e) {
+            console.error(`Failed to start sunshine: ${e}`);
+        }
     }
 
     _stopSunshine() {
-        GLib.spawn_command_line_async(`pkill -x ${SUNSHINE_PROCESS_NAME}`);
-        console.info('Sunshine stopped');
-        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 2, () => {
-            this._refreshStatus();
-            return GLib.SOURCE_REMOVE;
-        });
+        if (this._process) {
+            try {
+                this._process.force_exit();
+                this._running = false;
+                this._connected = false;
+                this._updateVisuals();
+            } catch (e) {
+                console.error(`Error stopping sunshine: ${e}`);
+            }
+            this._process = null;
+        }
     }
 
-    _isClientConnected() {
-        // Placeholder check for connection - replace with a real one if available
-        return false;
-    }
+    _updateVisuals() {
+        this._icon.remove_style_class_name('sunshine-icon-off');
+        this._icon.remove_style_class_name('sunshine-icon-started');
+        this._icon.remove_style_class_name('sunshine-icon-connected');
 
-    _statusCheckLoop() {
-        GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 10, () => {
-            this._refreshStatus();
-            return true;
-        });
+        if (!this._running) {
+            this._icon.icon_name = 'network-offline-symbolic';
+            this._icon.add_style_class_name('sunshine-icon-off');
+        } else if (this._connected) {
+            this._icon.icon_name = 'network-wired-symbolic';
+            this._icon.add_style_class_name('sunshine-icon-connected');
+        } else {
+            this._icon.icon_name = 'weather-clear-symbolic';
+            this._icon.add_style_class_name('sunshine-icon-started');
+        }
+
+        this._buildMenu();
     }
 });
 
-export default class SunshineStatusExtension extends Extension {
+export default class SunshineExtension extends Extension {
     enable() {
-        this._indicator = new Indicator();
+        this._indicator = new SunshineIndicator();
         Main.panel.addToStatusArea(this.uuid, this._indicator);
     }
 
     disable() {
-        this._indicator.destroy();
-        this._indicator = null;
+        if (this._indicator) {
+            this._indicator.destroy();
+            this._indicator = null;
+        }
     }
 }
